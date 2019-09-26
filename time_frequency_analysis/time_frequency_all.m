eae226c0cd95f3021ae342053eafecb34cfe83d4
James Bonaiuto <jbonaiuto@gmail.com>
2019-09-26 11:41:07 +0200

    added new functions

--- time_frequency_analysis/time_frequency_all.m
+++ time_frequency_analysis/time_frequency_all.m
@@ -8,8 +8,18 @@ compute_timefreqs_significance(study_info);
 
 compute_timefreqs_significance_btw_conditions(study_info);
 
+compute_absolute_relative_power(study_info);
+
+compute_cluster_absolute_relative_power(study_info);
+
+export_erd_data(study_info, study_info.erdplot_woi);
+
+plot_scalp_topo_time_range(study_info, study_info.topoplot_woi);
+
 plot_time_frequency(study_info);
 
 plot_time_frequency_significance(study_info);
 
-plot_scalp_array_time_frequency(study_info)
+plot_scalp_array_time_frequency(study_info);
+
+plot_erds(study_info, study_info.erdplot_woi);
